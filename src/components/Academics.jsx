6922497915ab39bddfd94cd06ad296cf84088948
sakthi874 <srisakthidevi28@gmail.com>
2026-03-23 23:34:53 +0530

    Updated Academics section

--- src/components/Academics.jsx
+++ src/components/Academics.jsx
@@ -6,15 +6,19 @@ function Academics() {
            <br></br>
             <div >
                 <div className="Academics-item" >
-                    <h3>Master of Computer Applications (MCA)</h3>
-                    <p >kalasalingam Academy of Research and Education | 2023 - 2025</p>
+                    <h3>Master of Computer Applications (MCA)     |  83%   </h3>
+                    <p >kalasalingam Academy of Research and Education | Pursuing</p>
                 </div>
                 <div className="Academics-item" >
-                    <h3 >Bachelor of Computer Applications (BCA)</h3>
+                    <h3 >Bachelor of Computer Applications (BCA)   |  80.9%</h3>
                     <p >kalasalingam Academy of Research and Education | 2020 - 2023</p>
                 </div>
                 <div className="Academics-item" >
-                    <h3 >Higher Secondary Education</h3>
+                    <h3 >Higher Secondary Education  |  61%</h3>
+                    <p >Kshatriya Girl's Higher Secondary School | 2019- 2020</p>
+                </div>
+                <div className="Academics-item" >
+                    <h3 >SSLC  |  70%</h3>
                     <p >Kshatriya Girl's Higher Secondary School | 2019- 2020</p>
                 </div>
             </div>
